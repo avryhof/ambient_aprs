4a8cf72dc54de5cec356e01932a88aa0443a1ecc
avryhof <amos@vryhofresearch.com>
2018-10-02 11:31:06 -0400

    Initial Commit with testing scripts

---
+++ api_test.py
@@ -0,0 +1,14 @@
+import pprint
+
+from ambient_api.ambientapi import AmbientAPI
+
+weather = AmbientAPI()
+
+devices = weather.get_devices()
+
+for device in devices:
+    print(str(device))
+
+    pprint.pprint(device.last_data)
+
+    pprint.pprint(device.get_data())
---
+++ test.py
@@ -0,0 +1,102 @@
+# Script from https://raspberrypiandstuff.wordpress.com/2017/08/07/uploading-aprscwop-weather-data/
+
+import sys, os, time
+from datetime import datetime, timedelta
+from socket import *
+
+from ambient_api.ambientapi import AmbientAPI
+
+station_id = 'KD2OTL'
+
+serverHost = 'cwop.aprs.net'
+serverPort = 14580
+address = '%s>APRS,TCPIP*:' % station_id
+
+# Attention, format of the location is bit special. Although there is a dot, the values are in degrees, minutes and seconds!
+position = 'DDMM.SSN/DDDMM.SSE_'
+
+
+def hg_to_mbar(hg_val):
+    """
+    Convert inches of mercury (inHg to tenths of millibars/tenths of hPascals (mbar/hPa)
+    :param hg_val: The value in inHg
+    :return:
+    """
+    return (hg_val / 0.029529983071445) / 10
+
+
+
+def send_packet():
+    # here is where the big change happens.... rather than using rrdtool, we will just use the API
+    amb_api = AmbientAPI()
+    devices = amb_api.get_devices()
+    device = devices[0]
+    weather = device.last_data
+
+    # Prepare the data, which will be sent
+    wx_data = make_aprs_wx(
+        wind_dir=weather.get('winddir'),
+        wind_speed=float(weather.get('windspeedmph')),
+        wind_gust=float(weather.get('windgustmph')),
+        temperature=weather.get('tempf'),
+        rain_last_hr=weather.get('hourlyrainin'),
+        rain_last_24_hrs=None,
+        rain_since_midnight=weather.get('dailyrainin'),
+        humidity=weather.get('humidity'),
+        # Attention, barometric pressure in tenths of millibars/tenths of hPascal!
+        pressure=hg_to_mbar(weather.get('baromabsin'))
+    )
+    # Use UTC
+    utc_datetime = datetime.now()
+    # Create socket and connect to server
+    sSock = socket(AF_INET, SOCK_STREAM)
+    sSock.connect((serverHost, serverPort))
+    # Log on
+    sSock.send('user %s pass -1 vers Python\n' % station_id)
+    # Send packet
+    sSock.send(address + '@' + utc_datetime.strftime("%d%H%M") + 'z' + position + wx_data + 'Ambient APRS\n')
+    # Close socket, must be closed to avoid buffer overflow
+    sSock.shutdown(0)
+    sSock.close()
+
+
+def make_aprs_wx(**kwargs):
+    wind_dir = kwargs.get('wind_dir', None)
+    wind_speed = kwargs.get('wind_speed', None)
+    wind_gust = kwargs.get('wind_gust', None)
+    temperature = kwargs.get('temperature', None)
+    rain_last_hr = kwargs.get('rain_last_hr', None)
+    rain_last_24_hrs = kwargs.get('rain_last_24_hrs', None)
+    rain_since_midnight = kwargs.get('rain_since_midnight', None)
+    humidity = kwargs.get('humidity', None)
+    pressure = kwargs.get('pressure', None)
+
+    # Assemble the weather data of the APRS packet
+    def str_or_dots(number, length):
+        # If parameter is None, fill with dots, otherwise pad with zero
+        if number is None:
+            return '.' * length
+        else:
+            format_type = {
+                'int': 'd',
+                'float': '.0f',
+            }[type(number).__name__]
+            return ''.join(('%0', str(length), format_type)) % number
+
+    return '%s/%sg%st%sr%sp%sP%sh%sb%s' % (
+        str_or_dots(wind_dir, 3),
+        str_or_dots(wind_speed, 3),
+        str_or_dots(wind_gust, 3),
+        str_or_dots(temperature, 3),
+        str_or_dots(rain_last_hr, 3),
+        str_or_dots(rain_last_24_hrs, 3),
+        str_or_dots(rain_since_midnight, 3),
+        str_or_dots(humidity, 2),
+        str_or_dots(pressure, 5),
+    )
+
+
+try:
+    send_packet()
+except:
+    sys.exit(-1)
