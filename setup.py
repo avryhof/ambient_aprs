62d000487933e45fff1eca878bb593ee8c74b6f6
Amos Vryhof <amos@vryhofresearch.com>
2019-09-12 10:56:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ambient_aprs',
-    version='0.11.1',
+    version='0.11.2',
     packages=['ambient_aprs'],
     url='https://github.com/avryhof/ambient_aprs',
     license='MIT',
