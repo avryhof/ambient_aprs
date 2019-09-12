c70ceba597a7637f1e1f0534d8d931789f86d48f
Amos Vryhof <amos@vryhofresearch.com>
2019-09-12 10:55:12 -0400

    Update setup.py
    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ambient_aprs',
-    version='0.11.0',
+    version='0.11.1',
     packages=['ambient_aprs'],
     url='https://github.com/avryhof/ambient_aprs',
     license='MIT',
